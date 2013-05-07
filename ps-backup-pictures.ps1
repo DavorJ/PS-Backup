ca50ea58388c721c159499980ec0a7a9001f5571
Davor Josipovic <davorj@live.com>
2013-05-07 11:38:24 +0200

    Now some other properties are also tried if DateTimeOriginal doesn't exist

--- ps-backup-pictures.ps1
+++ ps-backup-pictures.ps1
@@ -36,12 +36,32 @@ foreach {
 } | 
 	
 foreach {
-	$pic = New-Object System.Drawing.Bitmap($_.FullName);
-	$str = [System.Text.Encoding]::ASCII.GetString($pic.GetPropertyItem(36867).Value);
+	$file = $_
+	$pic = New-Object System.Drawing.Bitmap($file.FullName);
+	
+	try {
+		# Property 36867 is DateTimeOriginal: http://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif/datetimeoriginal.html
+		$bytes = $pic.GetPropertyItem(36867).Value;
+	} catch {
+		try {
+			# Property 36868 is DateTimeDigitized: http://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif/datetimedigitized.html
+			$bytes = $pic.GetPropertyItem(36868).Value;
+		} catch {
+			try  {
+				# Property 306 is DateTime: http://www.awaresystems.be/imaging/tiff/tifftags/datetime.html
+				$bytes = $pic.GetPropertyItem(306).Value;
+			} catch {
+				Write-Host "EXIF Property can not be found on $($file.FullName)" -ForegroundColor "Magenta";
+				break;
+			}
+		}
+	}
+	
+	$str = [System.Text.Encoding]::ASCII.GetString($bytes);
 	$DateTaken=[datetime]::ParseExact($str,"yyyy:MM:dd HH:mm:ss`0",$Null);
 	$TargetPath = $Destination + "\" + $DateTaken.Year + "\" + $DateTaken.tostring("yyyy-MM-dd");
 
-	robocopy """$($_.DirectoryName)""" """$TargetPath""" """$($_.Name)""" /V /FP /NDL /NS /NP /R:1 /NJH /NJS | 
+	robocopy """$($file.DirectoryName)""" """$TargetPath""" """$($file.Name)""" /V /FP /NDL /NS /NP /R:1 /NJH /NJS | 
 	foreach {
 		if ($_) {
 			$line = $_.TrimStart();
